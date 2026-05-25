ae19f7271927b51c796da5d342f17925dc21da44
ARAVIND K KRISHNA <99062374+AravindKKrishna@users.noreply.github.com>
2026-05-25 10:59:30 +0530

    Update data_validation.py

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -109,6 +109,7 @@ class DataValidation:
             return data_validation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+            print("completed")
 
 
 
